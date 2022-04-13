6cafaf6037468e45daa135323a6b6f0b4fe8d6c5
Jonathan Toledo Moya <pablotoledomoya@gmail.com>
2022-04-12 23:49:59 -0400

    add react project and dependencies

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import './App.css';
+import 'web-components-review-module-table'
+import 'web-components-review-module-loremipsum'
+
+function App() {
+  const [childModule, setChildModule] = useState(null);
+  const [childWidth, setChildWidth] = useState('');
+  const [textAlign, setTextAlign] = useState('');
+
+  return (
+    <div className="App">
+      <h1>Container project</h1>
+
+      <h2>Children attributes</h2>
+      
+      <section>
+        <div>
+          Width:
+          <input
+            onChange={(event) => setChildWidth(event.target.value)}
+            value={childWidth}
+          />
+          %
+        </div>
+        <div>
+          Text align:
+          <select
+            onChange={(event) => setTextAlign(event.target.value)}
+            value={textAlign}
+          >
+            <option value="center">Center</option>
+            <option value="justify">Justify</option>
+            <option value="left">Left</option>
+            <option value="Right">Right</option>
+          </select>
+        </div>
+      </section>
+
+      <h2>Select any module from the menu</h2>
+      <ul>
+        <li><a href="#" onClick={() => setChildModule('module1')}>HTML Table example</a></li>
+        <li><a href="#" onClick={() => setChildModule('module2')}>Lorem Impsum example</a></li>
+      </ul>
+
+      <div>
+        {childModule === 'module1' ? (
+          <table-module width={childWidth}></table-module>
+        ) : null}
+
+        {childModule === 'module2' ? (
+          <lorem-ipsum-module width={childWidth} text-align={textAlign}></lorem-ipsum-module>
+        ) : null}
+      </div>
+    </div>
+  );
+}
+
+export default App;
